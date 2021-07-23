cc1bd713aa551bc67b5466d18f0a6ecdbadf2959
Ibrahim <31188911+speedercoder@users.noreply.github.com>
2021-07-23 15:39:50 +0400

    Initial file - incomplete
    The basic structure has been laid out. However, all of the code has not been written

---
+++ Guess the number game.py
@@ -0,0 +1,10 @@
+num = 1  # add random number generator
+x = 1
+guess = input("Enter a number from 0 to 100: ")
+
+# while loop
+
+# if statement
+if guess == num:
+    print("You guessed the number correctly")
+    print("Number of tries " + x)
